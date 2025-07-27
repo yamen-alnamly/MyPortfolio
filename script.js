2d9cb1c338aad04b4d1586a665addd356a895715
Yamen <125800880+yamen-alnamly@users.noreply.github.com>
2025-07-27 19:53:58 +0300

    Add files via upload
    finished the portfolio website and made it responsive

--- script.js
+++ script.js
@@ -50,16 +50,21 @@ ContactButton.onclick = () => {
 
 new Swiper('.cardWrapper', {
     loop: true,
+    
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable:true,
+     
     },
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }, 
+    },
+    
+   
   });
 
